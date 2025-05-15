24d721a0120d943334fbd94915c0da04f5f2bb6c
BraxtonGoode <tjbgoode@gmail.com>
2025-05-15 15:28:31 -0700

    added swagger and put it in swagger output

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use('/', require('./routes'));
 
 mongodb.initDb(err => {
